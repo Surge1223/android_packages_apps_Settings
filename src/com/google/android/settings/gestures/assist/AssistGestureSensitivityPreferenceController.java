56cb9a442af2266990838561ebebf3f3aa18015a
Surge Raval <Surge1223@gmail.com>
2018-07-07 02:43:09 +0000

    ActiveEdge: id clean up and complete Training additions

--- src/com/google/android/settings/gestures/assist/AssistGestureSensitivityPreferenceController.java
+++ src/com/google/android/settings/gestures/assist/AssistGestureSensitivityPreferenceController.java
@@ -17,6 +17,8 @@ import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
 import android.text.TextUtils;
 import android.util.Log;
+import com.android.settings.R;
+
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settings.gestures.AssistGestureFeatureProvider;
 import com.android.settings.overlay.FeatureFactory;
