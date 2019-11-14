96869d6782abefc6dfc83dc10171b81d70bca049
Surge Raval <Surge1223@gmail.com>
2019-11-14 08:16:32 +0100

    get rid of more old stuff

--- src/com/google/android/settings/gestures/assist/AssistGestureFeatureProviderGoogleImpl.java
+++ src/com/google/android/settings/gestures/assist/AssistGestureFeatureProviderGoogleImpl.java
@@ -3,98 +3,64 @@ package com.google.android.settings.gestures.assist;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PermissionInfo;
 import android.content.res.Resources;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
+import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.app.AssistUtils;
-import com.android.internal.widget.ILockSettings.Stub;
+import com.android.internal.widget.ILockSettings;
+import com.android.settings.R;
 import com.android.settings.gestures.AssistGestureFeatureProviderImpl;
-import com.android.settingslib.core.AbstractPreferenceController;
-import com.android.settingslib.core.lifecycle.Lifecycle;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class AssistGestureFeatureProviderGoogleImpl extends AssistGestureFeatureProviderImpl {
-  private static String TAG = AssistGestureFeatureProviderGoogleImpl.class.getSimpleName();
-
-  private static boolean hasAssistGestureSensor(Context context) {
-    String Elmyra = "com.google.sensor.elmyra.sensor";
-    for (Sensor stringType :
-        ((SensorManager) context.getSystemService(Context.SENSOR_SERVICE))
-            .getSensorList(Sensor.TYPE_ALL)) {
-      if (stringType.getStringType().equals(Elmyra)) {
-        Log.w(TAG, " Found Sensor");
-        return true;
-      }
+    public boolean isSupported(Context context) {
+        return hasAssistGestureSensor(context) && isGsaCurrentAssistant(context) && isOpaEligible(context) && isOpaEnabled(context);
     }
-    Log.w(TAG, " Didnt find sensor");
-    return false;
-  }
 
-  private static boolean isGsaCurrentAssistant(Context context) {
-    ComponentName assistComponentForUser =
-        new AssistUtils(context).getAssistComponentForUser(UserHandle.USER_CURRENT);
-    return true;
-  }
-
-  private static boolean isOpaEligible(Context context) {
-    return Settings.Secure.getIntForUser(
-            context.getContentResolver(), "systemui.google.opa_enabled", 0, UserHandle.USER_CURRENT)
-        != 0;
-  }
-
-  public static boolean isOpaEnabled(Context context) {
-    boolean z = false;
-    try {
-      z =
-          Stub.asInterface(ServiceManager.getService("lock_settings"))
-              .getBoolean("systemui.google.opa_user_enabled", false, UserHandle.USER_CURRENT);
-    } catch (Throwable e) {
-      Log.e("AssistGestureFeatureProviderGoogleImpl", "isOpaEnabled RemoteException", e);
+    public boolean isSensorAvailable(Context context) {
+        return hasAssistGestureSensor(context);
     }
-    return true;
-  }
 
-  public List<AbstractPreferenceController> getControllers(Context context, Lifecycle lifecycle) {
-    List arrayList = new ArrayList();
-    arrayList.add(new AssistGesturePreferenceController(context, lifecycle));
-    arrayList.add(new AssistGestureSilenceAlertsPreferenceController(context, lifecycle));
-    arrayList.add(new AssistGestureWakePreferenceController(context, lifecycle));
-    arrayList.add(new AssistGestureSensitivityPreferenceController(context, lifecycle));
-    return arrayList;
-  }
-
-  public boolean isDeskClockSupported(Context context) {
-    PackageManager packageManager = context.getPackageManager();
-    Resources resources = context.getResources();
-    try {
-      PermissionInfo permissionInfo =
-          packageManager.getPermissionInfo(
-              "com.google.android.deskclock.permission.RECEIVE_ALERT_BROADCASTS", 0);
-      return permissionInfo != null
-          && permissionInfo.packageName.equals("com.google.android.deskclock");
-    } catch (NameNotFoundException e) {
-      return false;
+    public boolean isDeskClockSupported(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        Resources resources = context.getResources();
+        try {
+            PermissionInfo permissionInfo = packageManager.getPermissionInfo(resources.getString(R.string.gesture_assist_deskclock_permission), 0);
+            if (permissionInfo == null || !permissionInfo.packageName.equals(resources.getString(R.string.gesture_assist_deskclock_package))) {
+                return false;
+            }
+            return true;
+        } catch (PackageManager.NameNotFoundException unused) {
+        }
     }
-  }
 
-  public boolean isSensorAvailable(Context context) {
-    return hasAssistGestureSensor(context);
-  }
+    private static boolean isOpaEligible(Context context) {
+        if (Settings.Secure.getIntForUser(context.getContentResolver(), "systemui.google.opa_enabled", 0, -2) != 0) {
+            return true;
+        }
+        return false;
+    }
 
-  public boolean isSupported(Context context) {
-    return (hasAssistGestureSensor(context)
-            && isGsaCurrentAssistant(context)
-            && isOpaEligible(context))
-        && isOpaEnabled(context);
-  }
+    public static boolean isOpaEnabled(Context context) {
+        try {
+            return ILockSettings.Stub.asInterface(ServiceManager.getService("lock_settings")).getBoolean("systemui.google.opa_user_enabled", false, -2);
+        } catch (RemoteException e) {
+            Log.e("AssistGestureFeatureProviderGoogleImpl", "isOpaEnabled RemoteException", e);
+            return false;
+        }
+    }
+
+    private static boolean isGsaCurrentAssistant(Context context) {
+        String string = context.getResources().getString(R.string.gesture_assist_component);
+        ComponentName assistComponentForUser = new AssistUtils(context).getAssistComponentForUser(UserHandle.myUserId());
+        return assistComponentForUser != null && assistComponentForUser.flattenToString().equals(string);
+    }
+
+    private static boolean hasAssistGestureSensor(Context context) {
+        return context.getPackageManager().hasSystemFeature("android.hardware.sensor.assist");
+    }
 }
-
--- src/com/google/android/settings/gestures/assist/AssistGesturePreferenceController.java
+++ src/com/google/android/settings/gestures/assist/AssistGesturePreferenceController.java
@@ -1,40 +1,39 @@
 package com.google.android.settings.gestures.assist;
 
 import android.content.Context;
-import android.provider.Settings.Secure;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.text.TextUtils;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import com.android.internal.annotations.VisibleForTesting;
 import com.android.settings.gestures.AssistGestureFeatureProvider;
 import com.android.settings.gestures.GesturePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
-import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
-import com.android.settings.R;
 
 public class AssistGesturePreferenceController extends GesturePreferenceController implements LifecycleObserver, OnResume {
+    @VisibleForTesting
     static final int OFF = 0;
+    @VisibleForTesting
     static final int ON = 1;
+    private static final String PREF_KEY_VIDEO = "gesture_assist_video";
+    private static final String SECURE_KEY_ASSIST = "assist_gesture_enabled";
     private final AssistGestureFeatureProvider mFeatureProvider;
     private Preference mPreference;
     private PreferenceScreen mScreen;
 
-    public AssistGesturePreferenceController(Context context, Lifecycle lifecycle) {
-        super(context, lifecycle);
+    public AssistGesturePreferenceController(Context context, String str) {
+        super(context, str);
         this.mFeatureProvider = FeatureFactory.getFactory(context).getAssistGestureFeatureProvider();
-        if (lifecycle != null) {
-            lifecycle.addObserver(this);
-        }
     }
 
-    private void updatePreference() {
-        if (this.mPreference != null && this.mScreen != null) {
-            if (!isAvailable()) {
-                removePreference(this.mScreen, getPreferenceKey());
-            } else if (this.mScreen.findPreference(getPreferenceKey()) == null) {
-                this.mScreen.addPreference(this.mPreference);
-            }
-        }
+    public String getVideoPrefKey() {
+        return PREF_KEY_VIDEO;
+    }
+
+    public void onResume() {
+        updatePreference();
     }
 
     public void displayPreference(PreferenceScreen preferenceScreen) {
@@ -43,29 +42,28 @@ public class AssistGesturePreferenceController extends GesturePreferenceControll
         super.displayPreference(preferenceScreen);
     }
 
-    public String getPreferenceKey() {
-        return "gesture_assist";
+    public int getAvailabilityStatus() {
+        return this.mFeatureProvider.isSupported(this.mContext) ? 0 : 3;
     }
 
-    protected String getVideoPrefKey() {
-        return "gesture_assist_video";
+    public boolean isSliceable() {
+        return TextUtils.equals(getPreferenceKey(), "gesture_assist");
     }
 
-    public boolean isAvailable() {
-        return this.mFeatureProvider.isSupported(this.mContext);
+    public boolean isChecked() {
+        return Settings.Secure.getInt(this.mContext.getContentResolver(), SECURE_KEY_ASSIST, 1) != 0;
     }
 
-    protected boolean isSwitchPrefEnabled() {
-        return Secure.getInt(this.mContext.getContentResolver(), "assist_gesture_enabled", 1) != 0;
+    public boolean setChecked(boolean z) {
+        return Settings.Secure.putInt(this.mContext.getContentResolver(), SECURE_KEY_ASSIST, z ? 1 : 0);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object obj) {
-        Secure.putInt(this.mContext.getContentResolver(), "assist_gesture_enabled", ((Boolean) obj).booleanValue() ? 1 : 0);
-        return true;
-    }
-
-    public void onResume() {
-        updatePreference();
+    private void updatePreference() {
+        if (this.mPreference != null) {
+            PreferenceScreen preferenceScreen = this.mScreen;
+            if (preferenceScreen != null) {
+                setVisible(preferenceScreen, getPreferenceKey(), isAvailable());
+            }
+        }
     }
 }
-
